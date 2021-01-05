ecc549dc41f6e123dbb1fa3bce669fbde0329051
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-05 22:41:58 +0000

    Add JSON examples

--- app.js
+++ app.js
@@ -1,9 +1,57 @@
-document.querySelector('#button1').addEventListener('click', getCustomer);
-document.querySelector('#button2').addEventListener('click', getCustomers);
-function getCustomer() {
-  console.log('Get Customer...');
+document.querySelector('#button1').addEventListener('click', loadCustomer);
+document.querySelector('#button2').addEventListener('click', loadCustomers);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.send();
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+          <li>Github: ${customer.github}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
 }
 
- function getCustomers() {
-  console.log('Get Customers...');
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.send();
+  
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+      
+      customers.forEach((customer) => {
+        output += `
+          <ul>
+            <li>ID: ${customer.id}</li>
+            <li>Name: ${customer.name}</li>
+            <li>Company: ${customer.company}</li>
+            <li>Phone: ${customer.phone}</li>
+            <li>Github: ${customer.github}</li>
+          </ul>
+      `;
+      });
+      document.querySelector('#customers').innerHTML = output;
+    }
+  }
  }
