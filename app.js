872e681d4d67f7b7f665be17f2d957d484a1292a
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-05 22:04:16 +0000

    Add basic button click events

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('#button1').addEventListener('click', getCustomer);
+document.querySelector('#button2').addEventListener('click', getCustomers);
+function getCustomer() {
+  console.log('Get Customer...');
+}
+
+ function getCustomers() {
+  console.log('Get Customers...');
+ }
